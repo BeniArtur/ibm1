549a52dc32e5adfb84b65b68ef9ab63cae4dca47
BeniArtur <92297415+BeniArtur@users.noreply.github.com>
2022-07-26 14:10:49 +0300

    routing commit with '#' will be ignored, and an empty message aborts the commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { NgModule } from '@angular/core';
+import{ NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { VoteComponent } from './vote/vote.component';
+import {MatRadioModule} from '@angular/material/radio';
+import { RouterModule, Routes } from '@angular/router';
+const routes:Routes=[
+  {path:'vote',
+component:VoteComponent
+},
+{path:'dashboard',
+component:DashboardComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    DashboardComponent,
+    VoteComponent,
+
   ],
+ 
   imports: [
-    BrowserModule
+     BrowserModule,
+     RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatRadioModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/vote/vote.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+import {MatRadioModule} from '@angular/material/radio';
+
+@Component({
+  selector: 'vote.component',
+  templateUrl: 'vote.component.html',
+  styleUrls: ['vote.component.css'],
+})
+export class VoteComponent {}
